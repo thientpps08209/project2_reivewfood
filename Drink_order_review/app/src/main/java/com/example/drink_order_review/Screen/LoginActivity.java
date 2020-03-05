4d49d45d3b2d60a311ce00e984c80d730800df26
thientpps08209 <45841474+thientpps08209@users.noreply.github.com>
2020-03-05 15:13:30 +0700

    ok

---
+++ Drink_order_review/app/src/main/java/com/example/drink_order_review/Screen/LoginActivity.java
@@ -0,0 +1,16 @@
+package com.example.drink_order_review.Screen;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.drink_order_review.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
--- Drink_order_review/app/src/main/java/com/example/drink_order_review/Screen/MainActivity.java
+++ Drink_order_review/app/src/main/java/com/example/drink_order_review/Screen/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.drink_order_review.Screen;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 import com.example.drink_order_review.R;
 
@@ -12,5 +14,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        new Handler().postDelayed(new Runnable() {
+
+
+            @Override
+            public void run() {
+                Intent i = new Intent(MainActivity.this,LoginActivity.class);
+                startActivity(i);
+
+                finish();
+
+            }
+        },3000);
     }
 }
